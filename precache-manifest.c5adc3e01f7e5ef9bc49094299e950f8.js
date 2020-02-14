caabf24adfe0f98877e6802be0e57d279b7c972f
Senya Volkov <formobileprima@gmail.com>
2020-02-14 14:03:23 +0200

    Updates

--- precache-manifest.c5adc3e01f7e5ef9bc49094299e950f8.js
+++ precache-manifest.c5adc3e01f7e5ef9bc49094299e950f8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5b2f4004593b392ec8f28c217b1ff6a9",
+    "revision": "dd9a1ae63c76957bf5308c6281c35c01",
     "url": "./index.html"
   },
   {
-    "revision": "39a281afff96aa6bf52e",
+    "revision": "d8225a52800a74f9316f",
     "url": "./static/css/main.36497136.chunk.css"
   },
   {
-    "revision": "0916a0b2543e3c9540db",
-    "url": "./static/js/2.3b313163.chunk.js"
+    "revision": "919348022003544d4708",
+    "url": "./static/js/2.10212bdd.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "./static/js/2.3b313163.chunk.js.LICENSE"
+    "url": "./static/js/2.10212bdd.chunk.js.LICENSE"
   },
   {
-    "revision": "39a281afff96aa6bf52e",
-    "url": "./static/js/main.77e62cb9.chunk.js"
+    "revision": "d8225a52800a74f9316f",
+    "url": "./static/js/main.065fb40c.chunk.js"
   },
   {
     "revision": "ccbd7f10272ec99a0d7d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.c5adc3e01f7e5ef9bc49094299e950f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
